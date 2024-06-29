9277bb3e39fd8301ee68ef226332d93848687da1
s27444 <s27444@pjwstk.edu.pl>
2024-06-29 17:53:04 +0200

    utworzenie projektu

---
+++ jaz-s27444-nbp/src/main/java/com/example/jazs27444nbp/JazS27444NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs27444nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27444NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS27444NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s27444-nbp/src/test/java/com/example/jazs27444nbp/JazS27444NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs27444nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS27444NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
